8e02a6cde9e70cfdafac7bbde86aa8e4e9754f2d
Manuel Castanares <manuel.castanares14@gmail.com>
2021-11-18 19:41:01 -0300

    a

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
@@ -15,10 +11,3 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
-
-    
-# Create your views here.
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the tasks index.")
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-
 from . import views
+from django.urls import include, path
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
