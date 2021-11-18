9c9005c596a53d68d2a038b6a9a73661136de594
Manuel Castanares <manuel.castanares14@gmail.com>
2021-11-18 20:00:33 -0300

    aa

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,6 +5,7 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'index', views.index)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
